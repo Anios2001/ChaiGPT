1ab83bb13e31c183e9637bf593eef2df60ec57b8
Anios2001 <64782002+Anios2001@users.noreply.github.com>
2023-11-30 23:42:39 +0530

    Change in client file

--- audio_client.py
+++ audio_client.py
@@ -7,24 +7,21 @@ SERVER_PORT= 8081
 async def getFile():
     try:
         async with websockets.connect(f"ws://{SERVER_IP_ADDRESS}:{SERVER_PORT}") as connection:
-            file_name = input('Enter name of file to be requested from the server:\n')
-            await connection.send(file_name)
-            print(f'{file_name} Requested from the server....')
-
-            response= await connection.recv()
-            if response.startswith('FNF'.encode()):
-                print(f'File does not exist on server...')
-            else:
-                with open('recieved.m4a', 'wb') as audio_file:
-                    audio_file.write(response)    
+            file_name = input('Enter name of file to be send to the server:\n')
+            with open(file_name,'rb') as fileBytes:
+               await connection.send(fileBytes) 
+            transcription = await connection.recv()
+            print(transcription)      
     except websockets.ConnectionClosedOK:
         print(websockets.ConnectionClosedOK.reason)
+    except FileNotFoundError as e:
+        print(f"File not found ")    
     except PermissionError as e:
         print(f"Permission denied: {e}")
     except IsADirectoryError as e:
         print(f" The file is a directory : {e}")
     except TypeError as e:
-        print(f"Invalid data type for writing: {e}")
+        print(f"Invalid data type for reading: {e}")
     except IOError as e:
         print(f"Input/Output error occurred: {e}")
 

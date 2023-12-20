8970cd7a10c8320df257b4dd7d62dda835a93192
Anios2001 <aniketpoptani100@gmail.com>
2023-12-20 10:18:30 +0530

    Test 9

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ return fetch("/authenticate",
 //});
 //Fetch data Ops
 function submitForm(email, pass){
-  localStorage.setItem(email, pass);  
+  localStorage.setItem('ff', 'fuvh');  
   return authenticate({email:email, password:pass});
 }
 function checkResults(auth_token){
